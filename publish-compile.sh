451487949452e8ebb880ff020972ecc4500b0d61
shoothzj <shoothzj@gmail.com>
2021-08-28 12:12:49 +0800

    add compile go

--- publish-compile.sh
+++ publish-compile.sh
@@ -4,6 +4,7 @@ DIR="$( cd "$( dirname "$0"  )" && pwd  )"
 
 bash build-push-compile.sh c
 bash build-push-compile.sh c-ubuntu
+bash build-push-compile.sh go
 bash build-push-compile.sh jdk8
 bash build-push-compile.sh jdk11
 bash build-push-compile.sh jdk11-git
